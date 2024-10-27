9bbe2580ea582c0740229a18c37e69bf2302fc95
Liam Parry <khanh30125@gmail.com>
2024-10-27 22:42:53 +0700

    done update student information

--- main.c
+++ main.c
@@ -18,8 +18,7 @@ SV listSV[MAX];
 int stNum = 0;
 
 void addStudent();
-void editGrade();
-void editName();
+void updateStudent();
 void removeStudent();
 void sortGrade();
 void sortName();
@@ -39,16 +38,15 @@ int main()
         system("cls");
         printf("      ~ MENU ~\n");
         printf(" 1. Add student\n");
-        printf(" 2. Edit student grade\n");
-        printf(" 3. Edit student name\n");
-        printf(" 4. Remove student\n");
-        printf(" 5. Sort list by grade\n");
-        printf(" 6. Sort list by name\n");
-        printf(" 7. Print out all of list\n");
-        printf(" 8. Print out list by ID\n");
-        printf(" 9. Print out list by grade\n");
-        printf("10. Print out average, highest, lowest grade\n");
-        printf(" Other to Exit\n");
+        printf(" 2. Update student\n");
+        printf(" 3. Remove student\n");
+        printf(" 4. Sort list by grade\n");
+        printf(" 5. Sort list by name\n");
+        printf(" 6. Print out all of list\n");
+        printf(" 7. Print out list by ID\n");
+        printf(" 8. Print out list by grade\n");
+        printf(" 9. Print out average, highest, lowest grade\n");
+        printf(" 0. To Exit\n");
         readFromFile();
         printf("Please Choose: ");
         scanf("%d", &opt);
@@ -60,43 +58,47 @@ int main()
             addStudent();
             break;
         case 2:
-            editGrade();
+            updateStudent();
             break;
         case 3:
-            editName();
-            break;
-        case 4:
             removeStudent();
             break;
-        case 5:
+        case 4:
             sortGrade();
             break;
-        case 6:
+        case 5:
             sortName();
             break;
-        case 7:
+        case 6:
             printAll();
             break;
-        case 8:
+        case 7:
             printId();
             break;
-        case 9:
+        case 8:
             printGrade();
             break;
-        case 10:
+        case 9:
             printAverage();
             break;
         default:
+            system("cls");
             break;
         }
 
     } while (opt != 0);
 }
 
+void nameFormat(char *s)
+{
+}
+
 void addStudent()
 {
     // Enter main part of function
     system("cls");
+    printf("ADD STUDENT");
+    printf("\n-----------------------------------------\n");
     SV newSt;
     printf("Student ID: ");
     gets(newSt.IDStudent);
@@ -104,37 +106,129 @@ void addStudent()
     gets(newSt.LastName);
     printf("First name: ");
     gets(newSt.FirstName);
-    printf("Gender: ");
+    printf("Gender(Male|Female): ");
     gets(newSt.Gender);
     printf("Final Point: ");
     scanf("%f", &newSt.FinalPoint);
+    printf("\n-----------------------------------------\n");
 
-    listSV[stNum] = newSt;
+    char opt;
+    fflush(stdin);
+    printf("Are you sure? (Y/N): ");
+    scanf("%c", &opt);
 
-    printf("-----------------------------------------\n");
-    printf("Student %s is added successful!\n", listSV[stNum].IDStudent);
-    printf("-----------------------------------------\n");
-    stNum++;
-    saveToFile();
+    if (opt == 'y')
+    {
 
+        listSV[stNum] = newSt;
+
+        printf("\n-----------------------------------------\n");
+        printf("Student %s is added successful!\n", listSV[stNum].IDStudent);
+        printf("-----------------------------------------\n");
+        stNum++;
+        saveToFile();
+    }
+    else
+    {
+        printf("Process is cancelled!!\n");
+    }
     system("pause");
 
     return;
 }
-void editGrade()
-{
-    // Enter main part of function
-}
 
-void editName()
+void updateStudent()
 {
-    // Enter main part of function
+    system("cls");
+    printf("UPDATE STUDENT INFORMATION");
+    printf("\n-----------------------------------------\n");
+    char id[10];
+    printf("Search by ID: ");
+    gets(id);
+
+    int i;
+    for (i = 0; i < stNum; i++)
+    {
+        if (strcmp(listSV[i].IDStudent, id) == 0)
+        {
+            break;
+        }
+    }
+    if (i == stNum)
+    {
+        printf("\nID is not found!!\n");
+        system("pause");
+        return;
+    }
+
+    SV currentSt = listSV[i];
+    system("cls");
+    printf("UPDATE STUDENT INFORMATION");
+    printf("\n-----------------------------------------\n");
+    printf("%-10s %-20s %-8s %-10s\n", "ID", "Full Name", "Gender", "Grade");
+    char fullName[50] = "";
+    strcat(fullName, currentSt.FirstName);
+    strcat(fullName, " ");
+    strcat(fullName, currentSt.LastName);
+    printf("%-10s %-20s %-8s %-10.2f\n", currentSt.IDStudent, fullName, currentSt.Gender, currentSt.FinalPoint);
+    printf("\n-----------------------------------------\n");
+    printf("1. Edit ID\n");
+    printf("2. Edit name\n");
+    printf("3. Edit gender\n");
+    int opt;
+    scanf("\nChoose option: ");
+    scanf("%d", &opt);
+    fflush(stdin);
+    system("cls");
+    printf("UPDATE STUDENT INFORMATION");
+    printf("\n-----------------------------------------\n");
+    printf("%-10s %-20s %-8s %-10s\n", "ID", "Full Name", "Gender", "Grade");
+    printf("%-10s %-20s %-8s %-10.2f\n", currentSt.IDStudent, fullName, currentSt.Gender, currentSt.FinalPoint);
+    printf("\n-----------------------------------------\n");
+    switch (opt)
+    {
+    case 1:
+
+        printf("New ID: ");
+        gets(currentSt.IDStudent);
+        break;
+    case 2:
+
+        printf("New last name: ");
+        gets(currentSt.LastName);
+        printf("New first name: ");
+        gets(currentSt.FirstName);
+
+        break;
+    case 3:
+
+        printf("New gender: ");
+        gets(currentSt.Gender);
+
+        break;
+    default:
+        break;
+    }
+
+    printf("\n-----------------------------------------\n");
+
+    printf("Are you sure? (y/n)");
+    scanf("%c", &opt);
+    if (opt == 'y')
+    {
+        listSV[i] = currentSt;
+        saveToFile();
+    }
+
+    system("pause");
+    return;
 }
 
 void removeStudent()
 {
     int studentId;
     scanf("%d", studentId);
+    printf("%-10s %-20s %-8s %-10s\n", "ID", "Full Name", "Gender", "Grade");
 }
 
 void sortGrade()
@@ -200,23 +294,19 @@ void readFromFile()
 {
     stNum = 0;
     FILE *file = fopen("data.csv", "r");
-    if (file == NULL)
+    if (file != NULL)
     {
-        printf("Không thể mở file.\n");
-        exit(1);
+        char buffer[100];
+
+        while (fgets(buffer, sizeof(buffer), file) != NULL)
+        {
+            SV st;
+            sscanf(buffer, "%10[^,],%20[^,],%10[^,],%5[^,],%f", &st.IDStudent, &st.LastName, &st.FirstName, &st.Gender, &st.FinalPoint);
+
+            listSV[stNum] = st;
+
+            stNum++;
+        }
     }
-
-    char buffer[100];
-
-    while (fgets(buffer, sizeof(buffer), file) != NULL)
-    {
-        SV st;
-        sscanf(buffer, "%10[^,],%20[^,],%10[^,],%5[^,],%f", &st.IDStudent, &st.LastName, &st.FirstName, &st.Gender, &st.FinalPoint);
-
-        listSV[stNum] = st;
-
-        stNum++;
-    }
-
     fclose(file);
 }
